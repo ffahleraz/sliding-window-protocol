f9c2ea7edcc8257fe0f9f850086fa264953f2591
Faza Fahleraz <ffahleraz@gmail.com>
2018-10-29 22:05:42 +0700

    Implement sending data from external file with buffer

--- sendfile.cpp
+++ sendfile.cpp
@@ -8,7 +8,7 @@
 
 #include "helpers.h"
 
-#define TIMEOUT 10
+#define TIMEOUT 20
 
 #define current_time chrono::high_resolution_clock::now
 #define time_stamp chrono::high_resolution_clock::time_point
@@ -17,10 +17,6 @@
 using namespace std;
 
 int socket_fd;
-char *dest_ip;
-int dest_port;
-ssize_t buff_size;
-struct hostent *dest_hnet;
 struct sockaddr_in server_addr, client_addr;
 
 unsigned int window_size;
@@ -63,13 +59,20 @@ void listen_ack() {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc == 5) {
-        window_size = atoi(argv[1]);
-        buff_size = atoi(argv[2]);
-        dest_ip = argv[3];
-        dest_port = atoi(argv[4]);
+    char *dest_ip;
+    int dest_port;
+    ssize_t max_buffer_size;
+    struct hostent *dest_hnet;
+    char *fname;
+
+    if (argc == 6) {
+        fname = argv[1];
+        window_size = atoi(argv[2]);
+        max_buffer_size = atoi(argv[3]);
+        dest_ip = argv[4];
+        dest_port = atoi(argv[5]);
     } else {
-        cerr << "usage: ./sendfile <window_size> <buffer_size> <destination_ip> <destination_port>" << endl;
+        cerr << "usage: ./sendfile <filename> <window_size> <buffer_size> <destination_ip> <destination_port>" << endl;
         return 1; 
     }
 
@@ -107,67 +110,86 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    /* Start thread to listen for ack */
+    thread recv_thread(listen_ack);
+
+    /* Open file to send */
+    FILE *file = fopen(fname, "rb");
+    char *buffer;
+    buffer = new char[max_buffer_size];
+    size_t buffer_size;
+
+    /* Send file */
     char frame[MAX_FRAME_SIZE];
     char data[MAX_DATA_SIZE];
     size_t frame_size;
     size_t data_size;
-    
-    /* Initialize sliding window variables */
-    window_sent_time = new time_stamp[window_size];
-    window_ack_mask = new bool[window_size];
-    bool window_sent_mask[window_size];
-    
-    for (int i = 0; i < window_size; i++) {
-        window_sent_mask[i] = false;
-        window_ack_mask[i] = false;
-    }
 
-    /* Start thread to listen for ack */
-    thread recv_thread(listen_ack);
-
-    time_stamp huyu = current_time();
-
-    lar = -1;
-    lfs = lar + window_size;
     while (true) {
-        /* Check window ack mask, shift window if possible */
-        if (window_ack_mask[0]) {
-            unsigned int shift = 1;
-            for (unsigned int i = 1; i < window_size; i++) {
-                if (!window_ack_mask[i]) break;
-                shift += 1;
-            }
-            for (unsigned int i = 0; i < window_size - shift; i++) {
-                window_sent_mask[i] = window_sent_mask[i + shift];
-                window_ack_mask[i] = window_ack_mask[i + shift];
-                window_sent_time[i] = window_sent_time[i + shift];
-            }
-            for (unsigned int i = window_size - shift; i < window_size; i++) {
-                window_sent_mask[i] = false;
-                window_ack_mask[i] = false;
-            }
-            lar += shift;
-            lfs = lar + window_size;
+
+        /* Read part of file to buffer */
+        buffer_size = fread(buffer, 1, max_buffer_size, file);
+        
+        /* Initialize sliding window variables */
+        unsigned int seq_count = buffer_size / MAX_DATA_SIZE + ((buffer_size % MAX_DATA_SIZE == 0) ? 0 : 1);
+        unsigned int seq_num;
+        window_sent_time = new time_stamp[window_size];
+        window_ack_mask = new bool[window_size];
+        bool window_sent_mask[window_size];
+        for (int i = 0; i < window_size; i++) {
+            window_sent_mask[i] = false;
+            window_ack_mask[i] = false;
         }
 
-        for (unsigned int i = 0; i < window_size; i ++) {
-            if (!window_sent_mask[i] || (!window_ack_mask[i] && elapsed_time(current_time(), window_sent_time[i]) > TIMEOUT)) {
-                strcpy(data, "hello asu lu semua lkonto");
-                data_size = (size_t)(strlen(data) + 1);
-
-                unsigned int seq_num = lar + i + 1;
-                frame_size = create_frame(seq_num, frame, data, data_size);
-
-                sendto(socket_fd, frame, frame_size, MSG_CONFIRM, 
-                        (const struct sockaddr *) &server_addr, sizeof(server_addr));
-                window_sent_mask[i] = true;
-                window_sent_time[i] = current_time();
-
-                cout << "[SENT FRAME " << seq_num << "] " << data << endl;
+        lar = -1;
+        lfs = lar + window_size;
+        while (true) {
+            /* Check window ack mask, shift window if possible */
+            if (window_ack_mask[0]) {
+                unsigned int shift = 1;
+                for (unsigned int i = 1; i < window_size; i++) {
+                    if (!window_ack_mask[i]) break;
+                    shift += 1;
+                }
+                for (unsigned int i = 0; i < window_size - shift; i++) {
+                    window_sent_mask[i] = window_sent_mask[i + shift];
+                    window_ack_mask[i] = window_ack_mask[i + shift];
+                    window_sent_time[i] = window_sent_time[i + shift];
+                }
+                for (unsigned int i = window_size - shift; i < window_size; i++) {
+                    window_sent_mask[i] = false;
+                    window_ack_mask[i] = false;
+                }
+                lar += shift;
+                lfs = lar + window_size;
             }
+
+            for (unsigned int i = 0; i < window_size; i ++) {
+                seq_num = lar + i + 1;
+
+                if (seq_num < seq_count) {
+                    if (!window_sent_mask[i] || (!window_ack_mask[i] && elapsed_time(current_time(), window_sent_time[i]) > TIMEOUT)) {
+                        unsigned int buffer_shift = seq_num * MAX_DATA_SIZE;
+                        data_size = (buffer_size - buffer_shift < MAX_DATA_SIZE) ? (buffer_size - buffer_shift) : MAX_DATA_SIZE;
+                        memcpy(data, buffer + buffer_shift, data_size);
+                        frame_size = create_frame(seq_num, frame, data, data_size);
+
+                        sendto(socket_fd, frame, frame_size, MSG_CONFIRM, 
+                                (const struct sockaddr *) &server_addr, sizeof(server_addr));
+                        window_sent_mask[i] = true;
+                        window_sent_time[i] = current_time();
+
+                        cout << "[SENT FRAME " << seq_num << "] " << data << endl;
+                    }
+                }
+            }
+
+            if (lar >= seq_count - 1) break;
         }
 
-        if (lar >= 999) break;
+        cout << "huhuhuh" << endl;
+        if (buffer_size < max_buffer_size) break;
+        cout << "asjkdnajsdfa" << endl;
     }
     
     cout << "harusnya kelar gan" << endl;
